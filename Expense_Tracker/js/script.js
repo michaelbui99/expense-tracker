4749a78031a31113507c87e919826cbc9d407a2b
Michael Bui <michaelbui99@hotmail.com>
2020-12-25 19:17:48 +0100

    Fixed a bug where expenses with no input could be added

--- Expense_Tracker/js/script.js
+++ Expense_Tracker/js/script.js
@@ -16,7 +16,7 @@ function addExpense(event){
     let inputCurrency = document.querySelector("#currency").value;
 
     
-    if (!isNaN(inputAmount)){
+    if (!isNaN(inputAmount) && inputExpensename !== "" && inputExpensename !== null && inputAmount !== "" && inputAmount !== null && inputDate !== null && inputDate !== ""){
         expenseTable.innerHTML += "<tr><td>"+inputExpensename +"</td>"+ "<td>"+inputDate +"</td>"+ "<td>"+inputAmount+" " + inputCurrency +"</td>"+"<td><button class='remove-button'>Remove</button></td>"+"</tr>";
         document.querySelector("#amount").placeholder = 'Enter amount';
         document.querySelector("#amount").value = "";
@@ -27,8 +27,11 @@ function addExpense(event){
 
     }
     else{
-        document.querySelector("#amount").placeholder ="Invalid input, try again"
+        document.querySelector("#amount").placeholder ="Invalid input, try again";
         document.querySelector("#amount").value = "";
+        document.querySelector("#expenseName").placeholder = "Invalid input, try again";
+        document.querySelector("#expenseName").value = "";
+        document.querySelector("#date").value ="";
     }
     
 }
